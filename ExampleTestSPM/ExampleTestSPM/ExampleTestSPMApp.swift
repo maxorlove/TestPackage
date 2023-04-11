18dba11902865d1c92e1bdf6dfa9ca863645a473
Orlov Maxim <108331905+maxorlove@users.noreply.github.com>
2023-04-12 00:04:00 +0900

    Initial commit

---
+++ ExampleTestSPM/ExampleTestSPM/ExampleTestSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleTestSPMApp.swift
+//  ExampleTestSPM
+//
+//  Created by Orlov Maxim on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleTestSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
